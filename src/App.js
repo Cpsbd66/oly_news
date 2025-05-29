4499349cec7066a2630f5e0e4698f3229cd76217
Tapantor Das <tapantordas2008@gmail.com>
2025-05-29 17:57:36 +0600

    analytics

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "./firebase";
 import OlympiadTable from "./components/OlympiadTable";
 import MessageForm from "./components/MessageForm";
+// Import Analytics:
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   const [olympiads, setOlympiads] = useState([]);
@@ -13,36 +15,37 @@ function App() {
 
   useEffect(() => {
     (async () => {
-      const snapshot = await getDocs(collection(db, "olympiads"));
-      setOlympiads(snapshot.docs.map((doc) => doc.data()));
+      const snap = await getDocs(collection(db, "olympiads"));
+      setOlympiads(snap.docs.map(d => d.data()));
       setLoading(false);
     })();
   }, []);
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Container className="mt-5">
-            {loading ? (
-              <div className="text-center">
-                <Spinner color="primary" />
-              </div>
-            ) : (
-              <>
-                <OlympiadTable olympiads={olympiads} />
-                <Alert color="info" className="mt-4 text-center">
-                  Have feedback or want to request an event?{" "}
-                  <Link to="/contact">Send us a message!</Link>
-                </Alert>
-              </>
-            )}
-          </Container>
-        }
-      />
-      <Route path="/contact" element={<MessageForm />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Container className="mt-5">
+              {loading ? (
+                <Spinner color="primary" className="d-block mx-auto" />
+              ) : (
+                <>
+                  <OlympiadTable olympiads={olympiads} />
+                  <Alert color="info" className="mt-4 text-center">
+                    Have feedback? <Link to="/contact">Contact us!</Link>
+                  </Alert>
+                </>
+              )}
+            </Container>
+          }
+        />
+        <Route path="/contact" element={<MessageForm />} />
+      </Routes>
+
+      <Analytics />
+    </>
   );
 }
 
